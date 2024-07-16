7d8728bf8ab057cc40670039f862e5c6364a63dc
Lou <88289342+LouaiAlJabi@users.noreply.github.com>
2024-07-16 15:38:40 -0400

    finished the testing code

--- MoriZwanzigTesting.py
+++ MoriZwanzigTesting.py
@@ -1,5 +1,6 @@
 import Lou_MoriZwanzig as lmz
 import numpy as np
+import Lou_GeneralBootstrap as lbs
 
 velocity, force, sq_displacement = lmz.LoadDataFiles("velocities.npy", "forces.npy", "sq_displacement_stat.npy")
 newVel = np.array_split(velocity,20,1)[0]
@@ -14,3 +15,4 @@ time = np.linspace(0, dt*num_steps, num_steps)
 cutoffs = np.arange(1,29.)
 
 singleBrownian = lmz.BrownianParticleList(dataTuple,part_mass, dt,KB,T,Omega,start_fit_time,end_fit_time,time,cutoffs,num_steps)
+fullBoot = lbs.Bootstrap(lmz.BrownianParticleList,dataTuple,8,1,4,1,True,part_mass = part_mass, dt=dt,KB=KB,T=T,Omega=Omega,start_fit_time=start_fit_time,end_fit_time=end_fit_time,time=time,cutoffs=cutoffs,num_steps=num_steps)
